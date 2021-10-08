21477074c7ed26c36d71f3c146dc97b21b5bd1b8
bjd2385 <brandon.doyle@hqo.co>
2021-10-08 17:18:24 -0400

    Narrowing down the workflow

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,43 @@
+from typing import Dict, Any
+
 import tweepy
 import os
-import dotenv
+import requests
+
+from json import JSONDecodeError
 
 
 if API_KEY := os.getenv('API_KEY') is None:
     raise EnvironmentError('Must set API_KEY environment variable.')
+
 if SECRET_KEY := os.getenv('SECRET_KEY') is None:
     raise EnvironmentError('Must set SECRET_KEY environment variable.')
+
+
+def pull_astronaut_list(url: str ='http://api.open-notify.org/astros.json') -> Dict[str, Any]:
+    """
+    Pull a list of astronauts via API. Defaults to open-notify's API.
+
+    Args:
+        url: the URL to pull data from.
+
+    Returns:
+
+    """
+    data = dict()
+
+    try:
+        data = requests.get(url).json()
+    except JSONDecodeError:
+        exit(1)
+
+    return data
+
+
+def tweet() -> None:
+    """
+    Tweet the number of astronauts and their names retrieved from the API.
+
+    Returns:
+        Nothing.
+    """
