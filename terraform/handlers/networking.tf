0105d7dd9aba25866b565aa189c74c5a17acbccc
Brandon Doyle <brandon.doyle@hqo.co>
2021-10-10 17:47:19 -0400

    Convert vpc data to resource block

--- terraform/handlers/networking.tf
+++ terraform/handlers/networking.tf
@@ -1,6 +1,7 @@
-data "aws_vpc" "default" {
-  default = false
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+
 }
 
 # Ensure the selected availability zones actually exist.
