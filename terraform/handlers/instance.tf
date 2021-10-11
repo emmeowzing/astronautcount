dc65df03eeef187c38d7a1e1de163608ca60ab6d
Brandon Doyle <brandon.doyle@hqo.co>
2021-10-11 05:16:41 -0400

    Invalid type

--- terraform/handlers/instance.tf
+++ terraform/handlers/instance.tf
@@ -34,7 +34,7 @@ data "aws_iam_policy_document" "assume_role" {
     effect  = "Allow"
     principals {
       type        = "Service"
-      identifiers = "ec2.amazonaws.com"
+      identifiers = toset(["ec2.amazonaws.com"])
     }
   }
 }
