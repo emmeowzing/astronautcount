e09e55a91e21b32712b1538fd5b52de5eb6ed095
Brandon Doyle <brandon.doyle@hqo.co>
2021-10-11 06:10:44 -0400

    Add organization

--- terraform/circleci/circleci.tf
+++ terraform/circleci/circleci.tf
@@ -1,5 +1,6 @@
 resource "circleci_environment_variable" "public-eip" {
-  name    = "PUBLIC_EIP"
-  value   = var.public-eip
-  project = "astronautcount"
+  name         = "PUBLIC_EIP"
+  value        = var.public-eip
+  project      = "astronautcount"
+  organization = "bjd2385"
 }
