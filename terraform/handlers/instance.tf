0a777daaf537b814bced216aaee47f0aea24343d
Brandon Doyle <brandon.doyle@hqo.co>
2021-10-11 02:35:32 -0400

    Remove subnet specification

--- terraform/handlers/instance.tf
+++ terraform/handlers/instance.tf
@@ -47,7 +47,6 @@ resource "aws_launch_template" "astronautcount" {
     device_index = 0
     associate_public_ip_address = false
     security_groups = [aws_security_group.astronautcount-ingress.id]
-    subnet_id = aws_subnet.astronautcount.id
     network_interface_id = aws_network_interface.reused.id
   }
 }
