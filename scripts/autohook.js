d4972c5f7724714350ba98e8ce2d37af65ecfc3d
bjd2385 <brandon.doyle@hqo.co>
2021-11-25 14:54:51 -0500

    Add process env EIP

---
+++ scripts/autohook.js
@@ -0,0 +1,31 @@
+const Autohook = require('twitter-autohook');
+const http = require('http');
+const { URL } = require('url');
+
+const ip = http.ClientRequest().
+
+// https://github.com/twitterdev/autohook/blob/master/examples/standalone-server.js#L53
+(async () => {
+    try {
+      const webhookURL = new URL(process.env.PUBLIC_EIP);
+
+      const config = {
+        token: process.env.TWITTER_ACCESS_TOKEN,
+        token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        env: process.env.TWITTER_WEBHOOK_ENV,
+      };
+
+      const webhook = new Autohook(config);
+      await webhook.removeWebhooks();
+      await webhook.start(webhookURL);
+      await webhook.subscribe({
+        oauth_token: config.token,
+        oauth_token_secret: config.token_secret
+      });
+    } catch(e) {
+      console.error(e);
+      process.exit(-1);
+    }
+  })();
