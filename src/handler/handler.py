ba1bf3e6e94bc9a841c30479230113abf6c63e91
bjd2385 <brandon.doyle@hqo.co>
2021-11-26 16:41:48 -0500

    Wrapping in flask

--- src/handler/handler.py
+++ src/handler/handler.py
@@ -3,6 +3,7 @@
 
 
 from twitterwebhoooks import TwitterWebhookAdapter
+from flask import Flask
 
 import json
 import os
@@ -12,6 +13,14 @@ if (TWITTER_CONSUMER_SECRET := os.getenv('TWITTER_CONSUMER_SECRET')) is None:
     raise EnvironmentError('Must set SECRET_KEY environment variable.')
 
 events_adapter = TwitterWebhookAdapter(TWITTER_CONSUMER_SECRET, "/webhooks/astronautcount")
+app = Flask(__name__)
+
+
+@events_adapter.on("favorite_events")
+def favorite_events(event_data: dict) -> None:
+    event = event_data['event']
+    print(event)
+    print(json.dumps(event_data, indent=4, sort_keys=True))
 
 
 @events_adapter.on("tweet_create_events")
@@ -19,6 +28,3 @@ def handle_message(event_data: dict) -> None:
     event = event_data['event']
     print(event)
     print(json.dumps(event_data, indent=4, sort_keys=True))
-
-
-events_adapter.start(port=443)
