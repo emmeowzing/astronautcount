e5601ff099b5886f0758fc8cc72770f354d3a875
bjd2385 <brandon.doyle@hqo.co>
2021-11-27 19:31:53 -0500

    Correct version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='astronautcount',
-    version='0.1.0',
+    version='0.1.1',
     description='A Twitter bot that posts the number of astronauts in space every day.',
     long_description=long_description,
     long_description_content_type='text/markdown',
