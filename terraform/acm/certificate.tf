980ef4d7d0dd5ccde5738c5cd4ff1220f37621d1
bjd2385 <brandon.doyle@hqo.co>
2021-11-27 20:43:08 -0500

    Add additional records to route53 hosted zone

--- terraform/acm/certificate.tf
+++ terraform/acm/certificate.tf
@@ -24,6 +24,24 @@ resource "aws_acm_certificate" "astronautcount" {
   }
 }
 
+resource "aws_route53_record" "twitter-certs" {
+  for_each = {
+    for dvo in aws_acm_certificate.astronautcount.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = aws_route53_zone.domain.zone_id
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+}
+
 resource "aws_acm_certificate_validation" "astronautcount" {
   certificate_arn         = aws_acm_certificate.astronautcount.arn
   validation_record_fqdns = [aws_route53_record.twitter.fqdn]
