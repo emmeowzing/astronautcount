b87065b7e05fad8e0e7bbfab3cfa1d519864fc00
bjd2385 <brandon.doyle@hqo.co>
2021-11-27 20:55:13 -0500

    Minor corrections

--- terraform/acm/certificate.tf
+++ terraform/acm/certificate.tf
@@ -27,10 +27,9 @@ resource "aws_acm_certificate" "astronautcount" {
 resource "aws_route53_record" "twitter-certs" {
   for_each = {
     for dvo in aws_acm_certificate.astronautcount.domain_validation_options : dvo.domain_name => {
-      name    = dvo.resource_record_name
-      record  = dvo.resource_record_value
-      type    = dvo.resource_record_type
-      zone_id = aws_route53_zone.domain.zone_id
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
   }
 
@@ -39,12 +38,12 @@ resource "aws_route53_record" "twitter-certs" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = each.value.zone_id
+  zone_id         = aws_route53_zone.domain.zone_id
 }
 
 resource "aws_acm_certificate_validation" "astronautcount" {
   certificate_arn         = aws_acm_certificate.astronautcount.arn
-  validation_record_fqdns = [aws_route53_record.twitter.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.twitter-certs : record.fqdn]
 
   depends_on = [aws_route53_record.twitter-certs]
 }
